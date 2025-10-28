03f8229ab7db3f7183e1218db1d1cba3edc4e638
magodo <wztdyl@sina.com>
2025-10-28 17:39:49 +1100

    Use hcl-edit to parse the body, to conserve the structures/locations

--- src/lsp/document.rs
+++ src/lsp/document.rs
@@ -1,18 +1,18 @@
-use hcl::Body;
+use hcl_edit::{parser, structure};
 use lsp_document::{IndexedText, Pos, TextAdapter, TextMap};
 use tower_lsp::lsp_types::{
     Diagnostic, DiagnosticSeverity, NumberOrString, TextDocumentContentChangeEvent,
 };
 
 pub struct Document {
-    hcl_body: hcl::Result<Body>,
+    hcl_body: Result<structure::Body, parser::Error>,
     text: IndexedText<String>,
 }
 
 impl Document {
     pub fn new(text: &str) -> Self {
         let text = IndexedText::new(text.to_string());
-        let hcl_body = hcl::parse(text.text());
+        let hcl_body = parser::parse_body(text.text());
         Self { hcl_body, text }
     }
 
@@ -21,7 +21,7 @@ impl Document {
             panic!("Incremental change is not supported");
         }
         self.text = IndexedText::new(change.text.clone());
-        self.hcl_body = hcl::parse(self.text.text());
+        self.hcl_body = parser::parse_body(self.text.text());
         dbg!(&self.hcl_body);
     }
 
@@ -33,33 +33,29 @@ impl Document {
             return Vec::new();
         };
         tracing::debug!("parse error: {:#?}", err);
-        match err {
-            hcl::Error::Parse(err) => {
-                let loc = err.location();
-                // This range is zero indexed, hence needs to minus 1 from loc.
-                let range = std::ops::Range {
-                    start: Pos {
-                        line: (loc.line() - 1) as u32,
-                        col: (loc.column() - 1) as u32,
-                    },
-                    end: Pos {
-                        line: (loc.line() - 1) as u32,
-                        col: (err.line().len()) as u32,
-                    },
-                };
-                let range = self.text.range_to_lsp_range(&range).unwrap();
-                let diag = Diagnostic {
-                    range,
-                    severity: Some(DiagnosticSeverity::ERROR),
-                    code: Some(NumberOrString::String("parse".to_string())),
-                    source: Some("az-rs".to_string()),
-                    message: err.message().to_string(),
-                    ..Default::default()
-                };
-                tracing::debug!("diag: {diag:#?}");
-                return vec![diag];
-            }
-            _ => unreachable!("unexpected error: {err:#?}"),
-        }
+        let loc = err.location();
+
+        // The range below is zero indexed, hence needs to minus 1 from loc.
+        let range = std::ops::Range {
+            start: Pos {
+                line: (loc.line() - 1) as u32,
+                col: (loc.column() - 1) as u32,
+            },
+            end: Pos {
+                line: (loc.line() - 1) as u32,
+                col: (err.line().len()) as u32,
+            },
+        };
+        let range = self.text.range_to_lsp_range(&range).unwrap();
+        let diag = Diagnostic {
+            range,
+            severity: Some(DiagnosticSeverity::ERROR),
+            code: Some(NumberOrString::String("parse".to_string())),
+            source: Some("az-rs".to_string()),
+            message: err.message().to_string(),
+            ..Default::default()
+        };
+        tracing::debug!("diag: {diag:#?}");
+        return vec![diag];
     }
 }
